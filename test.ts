699d6277c75d57108cd4cd0898786efe8ca3e667
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-06-28 14:46:09 +0900

    fix tests

--- test.ts
+++ test.ts
@@ -2,20 +2,24 @@ import { assertEquals } from "https://deno.land/std@0.65.0/testing/asserts.ts";
 import { expandHome } from "./mod.ts";
 import { join } from "https://deno.land/std@0.145.0/path/mod.ts";
 
+const home = Deno.build.os === "windows"
+  ? Deno.env.get("USERPROFILE")!
+  : Deno.env.get("HOME")!;
+
 Deno.test("test simple #1", () => {
-  const want = Deno.env.get("HOME")!;
+  const want = home;
   const got = expandHome("~");
   assertEquals(got, want);
 });
 
 Deno.test("test simple #1", () => {
-  const want = join(Deno.env.get("HOME")!, "/");
+  const want = join(home, "/");
   const got = expandHome("~/");
   assertEquals(got, want);
 });
 
 Deno.test("test simple #3", () => {
-  const want = join(Deno.env.get("HOME")!, "/path/to");
+  const want = join(home, "/path/to");
   const got = expandHome("~/path/to");
   assertEquals(got, want);
 });
