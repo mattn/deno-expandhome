e8496d9e4440a19eb21dfb1c896bf5a294e6b654
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-06-28 14:41:14 +0900

    add test

--- test.ts
+++ test.ts
@@ -3,6 +3,18 @@ import { expandHome } from "./mod.ts";
 import { join } from "https://deno.land/std@0.145.0/path/mod.ts";
 
 Deno.test("test simple #1", () => {
+  const want = Deno.env.get("HOME")!;
+  const got = expandHome("~");
+  assertEquals(got, want);
+});
+
+Deno.test("test simple #1", () => {
+  const want = join(Deno.env.get("HOME")!, "/");
+  const got = expandHome("~/");
+  assertEquals(got, want);
+});
+
+Deno.test("test simple #3", () => {
   const want = join(Deno.env.get("HOME")!, "/path/to");
   const got = expandHome("~/path/to");
   assertEquals(got, want);
