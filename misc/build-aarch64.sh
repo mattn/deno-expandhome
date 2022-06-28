6951dc7e37c4c554cc83b00191575c47ddb6d71d
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-06-29 01:51:08 +0900

    fix

--- misc/build-aarch64.sh
+++ misc/build-aarch64.sh
@@ -5,10 +5,11 @@ FLAGS=
 
 case $os in
 linux)
+  FLAGS=-target aarch64-linux
   ;;
 darwin)
-  FLAGS=-F/host/Frameworks
+  FLAGS=-target aarch64-macos -F/host/Frameworks
   ;;
 esac
 
-ARCH=aarch64 CC="zig cc -target aarch64-${DENO_OS} ${FLAGS}" make clean dist
+ARCH=aarch64 CC="zig cc ${FLAGS}" make clean dist
